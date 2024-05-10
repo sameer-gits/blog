d4c4549ee1e26216fbfa231e3c86ae95ea6ff3a1
sameer-gits <ms3054825@gmail.com>
2024-05-10 23:00:51 +0530

    search is working now in homepage need to fix on posts page

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
 	"log"
 	"os"
@@ -16,6 +15,44 @@ import (
 	"golang.org/x/text/language"
 )
 
+func renderPosts(directory string) ([]map[string]interface{}, error) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []map[string]interface{}
+
+	for _, file := range files {
+		title := strings.TrimSuffix(file.Name(), ".md")
+		capitalTitle := cases.Title(language.Und, cases.NoLower).String(title)
+		markdown, err := os.ReadFile(directory + file.Name())
+		if err != nil {
+			log.Println("Error reading file:", err)
+			continue
+		}
+
+		md := goldmark.New(
+			goldmark.WithExtensions(extension.GFM),
+		)
+
+		var postContent bytes.Buffer
+		if err := md.Convert(markdown, &postContent); err != nil {
+			log.Println("Error converting markdown:", err)
+			continue
+		}
+		truncatedContent := postContent.String()
+		post := map[string]interface{}{
+			"Title":   capitalTitle,
+            "Content": template.HTML(truncatedContent[:200]),
+			"Slug":    strings.ReplaceAll(title, " ", "-"),
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
 func main() {
 	engine := html.New("./views", ".html")
 
@@ -23,93 +60,16 @@ func main() {
 		Views: engine,
 	})
 
+    directory := "./all_blogs/"
+
 	app.Static("/", "./public")
-
-	//below is search
-	app.Get("/search", func(c *fiber.Ctx) error {
-		queryParam := c.Query("query")
-		if queryParam == "" {
-			return c.SendStatus(fiber.StatusBadRequest)
-		}
-
-        fmt.Println(queryParam)
-		files, err := os.ReadDir("./all_blogs")
-		if err != nil {
-			log.Println("Error reading blog directory:", err)
-			return err
-		}
-
-		var posts []map[string]interface{}
-
-		for _, file := range files {
-			title := strings.TrimSuffix(file.Name(), ".md")
-			if strings.Contains(strings.ToLower(title), strings.ToLower(queryParam)) {
-				capitalTitle := cases.Title(language.Und, cases.NoLower).String(title)
-				markdown, err := os.ReadFile("./all_blogs/" + file.Name())
-				if err != nil {
-					log.Println("Error reading file:", err)
-					continue
-				}
-
-				md := goldmark.New(
-					goldmark.WithExtensions(extension.GFM),
-				)
-
-				var postContent bytes.Buffer
-				if err := md.Convert(markdown, &postContent); err != nil {
-					log.Println("Error converting markdown:", err)
-					continue
-				}
-				truncatedContent := postContent.String()
-				post := map[string]interface{}{
-					"Title":   capitalTitle,
-					"Content": template.HTML(truncatedContent[:200]),
-					"Slug":    strings.ReplaceAll(title, " ", "-"),
-				}
-				posts = append(posts, post)
-			}
-		}
-
-		return c.Render("post", fiber.Map{
-			"Posts": posts,
-		})
-	})
 
 	// Handler for the homepage to display all posts
 	app.Get("/", func(c *fiber.Ctx) error {
-		files, err := os.ReadDir("./all_blogs")
+		posts, err := renderPosts(directory)
 		if err != nil {
 			log.Println("Error reading blog directory:", err)
 			return err
-		}
-
-		var posts []map[string]interface{}
-
-		for _, file := range files {
-			title := strings.TrimSuffix(file.Name(), ".md")
-			capital_title := cases.Title(language.Und, cases.NoLower).String(title)
-			markdown, err := os.ReadFile("./all_blogs/" + file.Name())
-			if err != nil {
-				log.Println("Error reading file:", err)
-				continue
-			}
-
-			md := goldmark.New(
-				goldmark.WithExtensions(extension.GFM),
-			)
-
-			var postContent bytes.Buffer
-			if err := md.Convert(markdown, &postContent); err != nil {
-				log.Println("Error converting markdown:", err)
-				continue
-			}
-			truncated_content := postContent.String()
-			post := map[string]interface{}{
-				"Title":   capital_title,
-				"Content": template.HTML(truncated_content[:200]),
-				"Slug":    strings.ReplaceAll(title, " ", "-"),
-			}
-			posts = append(posts, post)
 		}
 
 		return c.Render("homepage", fiber.Map{
@@ -117,11 +77,34 @@ func main() {
 		})
 	})
 
+	// Handler for search
+	app.Get("/search", func(c *fiber.Ctx) error {
+		queryParam := c.Query("query")
+		posts, err := renderPosts(directory)
+		if err != nil {
+			log.Println("Error reading blog directory:", err)
+			return err
+		}
+
+		var filteredPosts []map[string]interface{}
+
+		for _, post := range posts {
+			title := post["Title"].(string)
+			if queryParam == "" || strings.Contains(strings.ToLower(title), strings.ToLower(queryParam)) {
+				filteredPosts = append(filteredPosts, post)
+			}
+		}
+
+		return c.Render("post", fiber.Map{
+			"Posts": filteredPosts,
+		})
+	})
+
 	// Handler for individual posts
 	app.Get("posts/:post_name?", func(c *fiber.Ctx) error {
 		postName := c.Params("post_name")
 		title := strings.ReplaceAll(postName, "-", " ")
-		capital_title := cases.Title(language.Und, cases.NoLower).String(title)
+		capitalTitle := cases.Title(language.Und, cases.NoLower).String(title)
 		if postName != "" {
 			markdown, err := os.ReadFile("./all_blogs/" + title + ".md")
 			if err != nil {
@@ -139,7 +122,7 @@ func main() {
 			}
 
 			return c.Render("post_templ", fiber.Map{
-				"Title":   capital_title,
+				"Title":   capitalTitle,
 				"Content": template.HTML(postContent.String()),
 			})
 		}
