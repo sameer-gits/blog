ea0a9dadf882528632b04888b0eff40da0d03869
sameer-gits <ms3054825@gmail.com>
2024-06-11 00:30:00 +0530

    added portfolio with database

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.2
 require (
 	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/gofiber/template/html/v2 v2.1.1
+	github.com/jackc/pgx/v5 v5.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/russross/blackfriday/v2 v2.1.0
 )
 
@@ -13,6 +15,9 @@ require (
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -21,5 +26,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,219 +1,276 @@
 package main
 
 import (
-    "bytes"
-    "html/template"
-    "log"
-    "net/mail"
-    "os"
-    "path/filepath"
-    "strings"
-    "sort"
-    "time"
+	"bytes"
+	"context"
+	"html/template"
+	"log"
+	"net/mail"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
 
-    "github.com/gofiber/fiber/v2"
-    fiberHTML "github.com/gofiber/template/html/v2"
-    "github.com/russross/blackfriday/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+
+	"github.com/gofiber/fiber/v2"
+	fiberHTML "github.com/gofiber/template/html/v2"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
-    primaryColor = "cyan"
-    directory    = "./all_blogs/"
-    dateLayout   = "02 Jan 2006"
+	primaryColor = "cyan"
+	directory    = "./all_blogs/"
+	dateLayout   = "02 Jan 2006"
 )
 
+type Work struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	Link        string `json:"link"`
+}
+
+var dbpool *pgxpool.Pool
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var err error
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	dbpool, err = pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+}
+
 func readMarkdownFile(filePath string) (map[string]string, string, error) {
-    markdown, err := os.ReadFile(filePath)
-    if err != nil {
-        return nil, "", err
-    }
+	markdown, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, "", err
+	}
 
-    msg, err := mail.ReadMessage(bytes.NewReader(markdown))
-    if err != nil {
-        return nil, "", err
-    }
+	msg, err := mail.ReadMessage(bytes.NewReader(markdown))
+	if err != nil {
+		return nil, "", err
+	}
 
-    metadata := map[string]string{
-        "Date":   msg.Header.Get("Date"),
-        "Author": msg.Header.Get("Author"),
-        "Title":  msg.Header.Get("Title"),
-        "Intro":  msg.Header.Get("Intro"),
-    }
+	metadata := map[string]string{
+		"Date":   msg.Header.Get("Date"),
+		"Author": msg.Header.Get("Author"),
+		"Title":  msg.Header.Get("Title"),
+		"Intro":  msg.Header.Get("Intro"),
+	}
 
-    buf := new(bytes.Buffer)
-    _, err = buf.ReadFrom(msg.Body)
-    if err != nil {
-        return nil, "", err
-    }
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(msg.Body)
+	if err != nil {
+		return nil, "", err
+	}
 
-    return metadata, buf.String(), nil
+	return metadata, buf.String(), nil
 }
 
 func searchPosts(directory string) ([]map[string]interface{}, error) {
-    files, err := os.ReadDir(directory)
-    if err != nil {
-        return nil, err
-    }
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
 
-    var posts []map[string]interface{}
+	var posts []map[string]interface{}
 
-    for _, file := range files {
-        if file.IsDir() || !strings.HasSuffix(file.Name(), ".md") {
-            continue
-        }
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".md") {
+			continue
+		}
 
-        title := strings.TrimSuffix(file.Name(), ".md")
-        filePath := filepath.Join(directory, file.Name())
+		title := strings.TrimSuffix(file.Name(), ".md")
+		filePath := filepath.Join(directory, file.Name())
 
-        metadata, _, err := readMarkdownFile(filePath)
-        if err != nil {
-            log.Println("Error reading file:", err)
-            continue
-        }
+		metadata, _, err := readMarkdownFile(filePath)
+		if err != nil {
+			log.Println("Error reading file:", err)
+			continue
+		}
 
-        post := map[string]interface{}{
-            "Metadata":     metadata,
-            "Slug":         strings.ReplaceAll(title, " ", "-"),
-            "PrimaryColor": primaryColor,
-        }
-        posts = append(posts, post)
-    }
+		post := map[string]interface{}{
+			"Metadata":     metadata,
+			"Slug":         strings.ReplaceAll(title, " ", "-"),
+			"PrimaryColor": primaryColor,
+		}
+		posts = append(posts, post)
+	}
 
-    return posts, nil
+	return posts, nil
 }
 
 func renderPosts(directory string) ([]map[string]interface{}, error) {
-    files, err := os.ReadDir(directory)
-    if err != nil {
-        return nil, err
-    }
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
 
-    var posts []map[string]interface{}
+	var posts []map[string]interface{}
 
-    for _, file := range files {
-        if file.IsDir() || !strings.HasSuffix(file.Name(), ".md") {
-            continue
-        }
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".md") {
+			continue
+		}
 
-        title := strings.TrimSuffix(file.Name(), ".md")
-        filePath := filepath.Join(directory, file.Name())
+		title := strings.TrimSuffix(file.Name(), ".md")
+		filePath := filepath.Join(directory, file.Name())
 
-        metadata, markdownContent, err := readMarkdownFile(filePath)
-        if err != nil {
-            log.Println("Error reading file:", err)
-            continue
-        }
+		metadata, markdownContent, err := readMarkdownFile(filePath)
+		if err != nil {
+			log.Println("Error reading file:", err)
+			continue
+		}
 
-        body := blackfriday.Run([]byte(markdownContent))
+		body := blackfriday.Run([]byte(markdownContent))
 
-        post := map[string]interface{}{
-            "Content":      template.HTML(body),
-            "Slug":         strings.ReplaceAll(title, " ", "-"),
-            "PrimaryColor": primaryColor,
-            "Metadata":     metadata,
-        }
-        posts = append(posts, post)
-    }
+		post := map[string]interface{}{
+			"Content":      template.HTML(body),
+			"Slug":         strings.ReplaceAll(title, " ", "-"),
+			"PrimaryColor": primaryColor,
+			"Metadata":     metadata,
+		}
+		posts = append(posts, post)
+	}
 
-    sort.Slice(posts, func(i, j int) bool {
-        dateI, errI := time.Parse(dateLayout, posts[i]["Metadata"].(map[string]string)["Date"])
-        dateJ, errJ := time.Parse(dateLayout, posts[j]["Metadata"].(map[string]string)["Date"])
+	sort.Slice(posts, func(i, j int) bool {
+		dateI, errI := time.Parse(dateLayout, posts[i]["Metadata"].(map[string]string)["Date"])
+		dateJ, errJ := time.Parse(dateLayout, posts[j]["Metadata"].(map[string]string)["Date"])
 
-        if errI != nil || errJ != nil {
-            log.Println("Error parsing dates:", errI, errJ)
-            return false
-        }
+		if errI != nil || errJ != nil {
+			log.Println("Error parsing dates:", errI, errJ)
+			return false
+		}
 
-        return dateI.After(dateJ)
-    })
+		return dateI.After(dateJ)
+	})
 
-    return posts, nil
+	return posts, nil
 }
 
 func main() {
+	defer dbpool.Close()
 
-    engine := fiberHTML.New("./views", ".html")
+	engine := fiberHTML.New("./views", ".html")
 
-    app := fiber.New(fiber.Config{
-        Views: engine,
-    })
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
-    app.Static("/", "./public")
+	app.Static("/", "./public")
 
-    // Handler for the homepage to display all posts
-    app.Get("/", func(c *fiber.Ctx) error {
-        posts, err := renderPosts(directory)
-        if err != nil {
-            log.Println("Error reading blog directory:", err)
-            return c.Status(fiber.StatusInternalServerError).SendString("Error reading blog directory")
-        }
+	// Handler for the homepage to display all posts
+	app.Get("/", func(c *fiber.Ctx) error {
+		posts, err := renderPosts(directory)
+		if err != nil {
+			log.Println("Error reading blog directory:", err)
+			return c.Status(fiber.StatusInternalServerError).SendString("Error reading blog directory")
+		}
 
-        return c.Render("homepage", fiber.Map{
-            "Posts":        posts,
-            "PrimaryColor": primaryColor,
-        })
-    })
+		return c.Render("homepage", fiber.Map{
+			"Posts":        posts,
+			"PrimaryColor": primaryColor,
+		})
+	})
 
-    // Handler for search
-    app.Get("/search", func(c *fiber.Ctx) error {
-        queryParam := strings.ToLower(c.Query("query"))
-        posts, err := searchPosts(directory)
-        if err != nil {
-            log.Println("Error reading blog directory:", err)
-            return c.Status(fiber.StatusInternalServerError).SendString("Error reading blog directory")
-        }
+	app.Get("/portfolio", func(c *fiber.Ctx) error {
+		rows, err := dbpool.Query(context.Background(), "SELECT id, title, description, image, link FROM work")
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to fetch work from database")
+		}
+		defer rows.Close()
 
-        var filteredPosts []map[string]interface{}
+		var works []Work
+		for rows.Next() {
+			var work Work
+			err := rows.Scan(&work.ID, &work.Title, &work.Description, &work.Image, &work.Link)
+			if err != nil {
+				return c.Status(fiber.StatusInternalServerError).SendString("Failed to scan work data")
+			}
+			works = append(works, work)
+		}
 
-        for _, post := range posts {
-            metadata := post["Metadata"].(map[string]string)
-            title := metadata["Title"]
+		if rows.Err() != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to iterate over work rows")
+		}
+		return c.Render("portfolio", fiber.Map{
+			"Works": works,
+		})
+	})
 
-            if queryParam == "" {
+	// Handler for search
+	app.Get("/search", func(c *fiber.Ctx) error {
+		queryParam := strings.ToLower(c.Query("query"))
+		posts, err := searchPosts(directory)
+		if err != nil {
+			log.Println("Error reading blog directory:", err)
+			return c.Status(fiber.StatusInternalServerError).SendString("Error reading blog directory")
+		}
 
-            } else if strings.Contains(strings.ToLower(title), strings.ToLower(queryParam)) {
-                // If the title contains the query string, include the post
-                filteredPosts = append(filteredPosts, post)
-            }
-        }
+		var filteredPosts []map[string]interface{}
 
-        return c.Render("post_results", fiber.Map{
-            "FilteredPosts": filteredPosts,
-            "PrimaryColor":  primaryColor,
-        })
-    })
+		for _, post := range posts {
+			metadata := post["Metadata"].(map[string]string)
+			title := metadata["Title"]
 
-    // Handler for individual posts
-    app.Get("/posts/:post_name?", func(c *fiber.Ctx) error {
-        postName := c.Params("post_name")
-        if postName != "" {
-            title := strings.ReplaceAll(postName, "-", " ")
-            filePath := filepath.Join(directory, title+".md")
+			if queryParam == "" {
 
-            metadata, markdownContent, err := readMarkdownFile(filePath)
-            if err != nil {
-                log.Println("Error reading file:", err)
-                return c.Status(fiber.StatusInternalServerError).SendString("Error reading file")
-            }
+			} else if strings.Contains(strings.ToLower(title), strings.ToLower(queryParam)) {
+				// If the title contains the query string, include the post
+				filteredPosts = append(filteredPosts, post)
+			}
+		}
 
-            body := blackfriday.Run([]byte(markdownContent))
+		return c.Render("post_results", fiber.Map{
+			"FilteredPosts": filteredPosts,
+			"PrimaryColor":  primaryColor,
+		})
+	})
 
-            return c.Render("post_single", fiber.Map{
-                "Content":      template.HTML(body),
-                "Metadata":     metadata,
-                "PrimaryColor": primaryColor,
-            })
-        }
+	// Handler for individual posts
+	app.Get("/posts/:post_name?", func(c *fiber.Ctx) error {
+		postName := c.Params("post_name")
+		if postName != "" {
+			title := strings.ReplaceAll(postName, "-", " ")
+			filePath := filepath.Join(directory, title+".md")
 
-        return c.SendStatus(fiber.StatusBadRequest)
-    })
+			metadata, markdownContent, err := readMarkdownFile(filePath)
+			if err != nil {
+				log.Println("Error reading file:", err)
+				return c.Status(fiber.StatusInternalServerError).SendString("Error reading file")
+			}
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "3000"
-    }
+			body := blackfriday.Run([]byte(markdownContent))
 
-    if err := app.Listen("0.0.0.0:" + port); err != nil {
-        log.Fatal("Server error:", err)
-    }
+			return c.Render("post_single", fiber.Map{
+				"Content":      template.HTML(body),
+				"Metadata":     metadata,
+				"PrimaryColor": primaryColor,
+			})
+		}
+
+		return c.SendStatus(fiber.StatusBadRequest)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	if err := app.Listen("0.0.0.0:" + port); err != nil {
+		log.Fatal("Server error:", err)
+	}
 }
